9b2b475050156e0886ef56809ad9b1ab37029776
Tani Umetsu <taniu@hawaii.edu>
2023-09-28 13:57:31 -1000

    Completed parts 1-7

--- HackwichFive/HackwichFive/ViewController.swift
+++ HackwichFive/HackwichFive/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()        // Do any additional setup after loading the view.
+        topLabel.text = "My Favorite Foods"
+
     }
-
-
+    
+    @IBAction func buttonOne(_ sender: Any) {
+        var currentIndex = 0
+        var favoriteFoodsArray : [String] = ["Panna Cotta", "Lasagna", "Tonkatsu", "Sushi", "Strawberry Shortcake"]
+        bottomLabel.text = favoriteFoodsArray[0]
+        bottomLabel.textColor = UIColor.black
+    
+    }
+    
 }
 
