0d55261fe8a03b6296f5f6b82bbb35a303035c6f
Tani Umetsu <taniu@hawaii.edu>
2023-10-04 20:30:16 -1000

    Completed Parts 1-8

--- HackwichFive/HackwichFive/ViewController.swift
+++ HackwichFive/HackwichFive/ViewController.swift
@@ -11,20 +11,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var buttonLabel: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()        // Do any additional setup after loading the view.
         topLabel.text = "My Favorite Foods:"
 
     }
+    var currentIndex = 0
     
     @IBAction func buttonOne(_ sender: Any) {
-        var currentIndex = 0
-        var favoriteFoodsArray : [String] = ["Panna Cotta", "Lasagna", "Tonkatsu", "Sushi", "Strawberry Shortcake"]
-        bottomLabel.text = favoriteFoodsArray[0]
-        bottomLabel.textColor = UIColor.black
-    
+        
+        let favoriteFoodsArray : [String] = ["Panna Cotta", "Lasagna", "Tonkatsu", "Sushi", "Strawberry Shortcake"]
+// #1 the favorite food arrawy variable to set the list of my favorite foods numbers 1-5
+        bottomLabel.text = favoriteFoodsArray[currentIndex]
+//#2 
+ 
+        if currentIndex < favoriteFoodsArray.count
+        {
+            buttonLabel.setTitle("Next", for:UIControl.State.normal)
+            currentIndex += 1
+        }
+            else {
+                print("button has been disabled")
+                (buttonLabel!).isEnabled=false
+            }
+
+        
+        
     }
+    
     
 }
 
--- HackwichFive/HackwichFive/secondViewController.swift
+++ HackwichFive/HackwichFive/secondViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class secondViewController: UIViewController {
-
+    
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
     
@@ -21,18 +21,24 @@ class secondViewController: UIViewController {
     @IBAction func buttonTwo(_ sender: Any) {
         var currentIndex = 0
         var favoriteShowsArray : [String] = ["911", "New Amsterdam", "Great British Baking Show", "Golden Girls", "Inuyasha"]
+        
         bottomLabel.text = favoriteShowsArray[2]
-        bottomLabel.textColor = UIColor.black
+        
+        if currentIndex < favoriteShowsArray.count {
+            bottomLabel.text = favoriteShowsArray[currentIndex]
+            currentIndex += 2
+            
+        }
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
